c51e869cd0a6996f454f275b0e554d8610cd9362
Justin Hammond <justin.35f@gmail.com>
2014-02-17 10:11:36 -0800

    Wrapped Performance.draw() in a context.save()/restore(). This fixed the context state change bug that affected text rendering.

--- firestorm/base/engine/Performance.js
+++ firestorm/base/engine/Performance.js
@@ -20,33 +20,35 @@ function Performance()
 
 	this.draw = function()
 	{
-		// Rect
-		Firestorm.context.fillStyle = "rgba(255, 255, 255, 0.25)";
-		Firestorm.context.fillRect(10, 10, 250, 120);
+		Firestorm.context.save();
+			// Rect
+			Firestorm.context.fillStyle = "rgba(255, 255, 255, 0.25)";
+			Firestorm.context.fillRect(10, 10, 250, 120);
 
-		// Text
-		Firestorm.context.font = '14pt Calibri';
-		Firestorm.context.textBaseline = 'top';
-		Firestorm.context.fillStyle = 'red';
-		Firestorm.context.fillText('FPS: ' + _AVERAGE_FPS, 15, 15);
+			// Text
+			Firestorm.context.font = '14pt Calibri';
+			Firestorm.context.textBaseline = 'top';
+			Firestorm.context.fillStyle = 'red';
+			Firestorm.context.fillText('FPS: ' + _AVERAGE_FPS, 15, 15);
 
-		// Line
-		Firestorm.context.beginPath();
-			Firestorm.context.strokeStyle = 'red';
-			Firestorm.context.moveTo(15, 40);
-			Firestorm.context.lineTo(255, 40);
-			Firestorm.context.stroke();
-		Firestorm.context.closePath();
+			// Line
+			Firestorm.context.beginPath();
+				Firestorm.context.strokeStyle = 'red';
+				Firestorm.context.moveTo(15, 40);
+				Firestorm.context.lineTo(255, 40);
+				Firestorm.context.stroke();
+			Firestorm.context.closePath();
 
-		// Wave
-		Firestorm.context.beginPath();
-			Firestorm.context.strokeStyle = 'red';
-			Firestorm.context.moveTo(255, 60);
-			for(var i = 0; i < 240; i++)
-			{
-				Firestorm.context.lineTo(255 - i, 120 - _FPS[i]);
-			}
-			Firestorm.context.stroke();
-		Firestorm.context.closePath();
+			// Wave
+			Firestorm.context.beginPath();
+				Firestorm.context.strokeStyle = 'red';
+				Firestorm.context.moveTo(255, 60);
+				for(var i = 0; i < 240; i++)
+				{
+					Firestorm.context.lineTo(255 - i, 120 - _FPS[i]);
+				}
+				Firestorm.context.stroke();
+			Firestorm.context.closePath();
+		Firestorm.context.restore();
 	}
 }
